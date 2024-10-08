d7774a65f86f8fabeeaa92428db13077ea918a52
too-noob-to-handle <98410308+too-noob-to-handle@users.noreply.github.com>
2024-10-08 13:27:12 +0600

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -7,13 +7,13 @@ class Telegram:
     ALLOWED_USER_IDS = env.get("ALLOWED_USER_IDS", "").split()
     BOT_USERNAME = env.get("TELEGRAM_BOT_USERNAME", "Archive_DDL")
     BOT_TOKEN = env.get("TELEGRAM_BOT_TOKEN", "7719746759:AAHpu-9K6Ymghsd-YK7PBw_BDR2TiTYWD-4")
-    CHANNEL_ID = int(env.get("TELEGRAM_CHANNEL_ID", -1002186699988))
+    CHANNEL_ID = int(env.get("TELEGRAM_CHANNEL_ID", -1001743701534)
     SECRET_CODE_LENGTH = int(env.get("SECRET_CODE_LENGTH", 12))
 
 class Server:
     BASE_URL = env.get("BASE_URL", "https://ddl.infinityfiles.eu.org")
     BIND_ADDRESS = env.get("BIND_ADDRESS", "0.0.0.0")
-    PORT = int(env.get("PORT", 8060))
+    PORT = int(env.get("PORT", 80))
 
 # LOGGING CONFIGURATION
 LOGGER_CONFIG_JSON = {
